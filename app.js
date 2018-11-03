babed41c056e8bee42fe681b43ac163d112c0198
TimmyJoseph <timitrilli@gmail.com>
2018-11-03 14:13:43 +0100

    progress

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ console.log('it wworks');
 $(document).ready(function (){
       $('.btn').click(function(event){
              event.preventDefault() 
-             console.log('Clicked btn');
+             console.log('Clicked button');
 
-             var email = $(".contact-email form-contro").val()
-             var subject = $(".contact-subject form-control").val()
-             var message = $(".contact-subject form-control").val()
+             var email = $('.email').val()
+             var subject = $('.subject').val()
+             var message = $('.message').val()
              var statusElm = $('.status')
              statusElm.empty()
 
+             
              if(email.length > 5 && email.includes('@') && email.includes('.')){
-                   statusElm.append('<div>Email is valid</div>')
-             } else{
-                  statusElm.append('<div>Email is not valid</div>')
-             }
+                  statusElm.append('<div>Email is valid</div>')
+            } else{
+                 statusElm.append('<div>Email is not valid</div>')
+            }
 
              if(subject.length > 2) {
                   statusElm.append('<div>Subject is valid</div>')
-             } else{
+             } else {
                   statusElm.append('<div>Subject is not valid</div>')
              }
 
